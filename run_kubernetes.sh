22fb17ad561b834242d0b9ad70431c0cf4c5e1f8
Ricky <support@nyrro.ch>
2022-09-29 16:08:53 +0200

    finished until step 7

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=nyrro/project-ml-python:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy project-ml-python --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get deploy,rs,svc,pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/project-ml-python-75466b69f-mbtpv --address 0.0.0.0 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=nyrro/project-ml-python:latest
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=nyrro
+docker tag project-ml-python $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
